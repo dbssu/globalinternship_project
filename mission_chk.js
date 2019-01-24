4f4fbb7883b5d594c8b92874797310fb5452a994
Yoonsu Kang <42951500+yoonsuKang03@users.noreply.github.com>
2019-01-24 11:38:22 +0900

    Add files via upload

--- mission_chk.js
+++ mission_chk.js
@@ -3,9 +3,11 @@ window.onload = function(){
     passform.onsubmit = function(){
         var passwd = document.getElementById("passwd").value;
         var answer = 1234;
+        var result = document.getElementById("result").innerHTML="";
 
         if(passwd == answer){
             alert("ok");
+            result = "비밀번호가 일치합니다.";
         }
         else{
             alert("nope");
